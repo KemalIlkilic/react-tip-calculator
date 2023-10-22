5caa3fff697a3c30682859cc82df0dbe36c789c2
KemalIlkilic <kemalonur.ilkilic@bahcesehir.edu.tr>
2023-10-22 19:03:08 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
@@ -8,4 +9,65 @@ export default function App() {
   );
 }
 
-function TipCalculator() {}
+function TipCalculator() {
+  const [bill, setBill] = useState("");
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
+  const tip = bill * ((percentage1 + percentage2) / 2 / 100);
+  return (
+    <div>
+      <BillInput bill={bill} onBill={setBill} />
+      <SelectInput percentage={percentage1} onPercentage={setPercentage1}>
+        How did you like the service ?{" "}
+      </SelectInput>
+      <SelectInput percentage={percentage2} onPercentage={setPercentage2}>
+        How did your friend like the service ?{" "}
+      </SelectInput>
+      {bill > 0 && (
+        <>
+          <Output bill={bill} tip={tip} />
+          {/* <Reset onReset={handleReset} /> */}
+        </>
+      )}
+    </div>
+  );
+}
+
+function BillInput({ bill, onBill }) {
+  return (
+    <div>
+      <label>How much was the bill?</label>
+      <input
+        type="text"
+        placeholder="Bill value"
+        value={bill}
+        onChange={(e) => onBill(Number(e.target.value))}
+      />
+    </div>
+  );
+}
+
+function SelectInput({ children, percentage, onPercentage }) {
+  return (
+    <div>
+      <label>{children}</label>
+      <select
+        value={percentage}
+        onChange={(e) => onPercentage(Number(e.target.value))}
+      >
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function Output({ bill, tip }) {
+  return (
+    <h3>
+      You pay ${bill + tip} (${bill} + ${tip} tip)
+    </h3>
+  );
+}
